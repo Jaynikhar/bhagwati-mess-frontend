83fb4df1f93d67cbc1306ff39d37d168f2963102
Jaynikhar <jaynikhar61@gmail.com>
2026-02-11 04:39:16 +0530

    edit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://bhagwati-mess.onrender.com",
+  baseURL: "https://bhagwati-mess.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
