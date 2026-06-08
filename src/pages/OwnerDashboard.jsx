7f13f9618f380fb0a7720ef1628c89b6fb6b061f
Jay <jaynikhar61@gmail.com>
2026-06-08 11:44:05 +0530

    Update OwnerDashboard.jsx

--- src/pages/OwnerDashboard.jsx
+++ src/pages/OwnerDashboard.jsx
@@ -3,60 +3,6 @@ import API from "../api/axios";
 import React from "react";
 import { useAuth } from "../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
-// export default function OwnerDashboard() {
-//   const [menu, setMenu] = useState({
-//     date: "",
-//     day: { rice: "", pulse: "", veggie: "", special: "" },
-//     night: { rice: "", pulse: "", veggie: "", special: "" },
-//   });
-
-//   const submitMenu = async () => {
-//     await API.post("/menu", menu);
-//     alert("Menu saved");
-//   };
-
-//   return (
-//     <div className="max-w-4xl mx-auto p-10 space-y-6">
-
-//       <div className="card">
-//         <h2 className="text-2xl font-bold">Create / Update Menu</h2>
-
-//         <input
-//           type="date"
-//           className="input mt-4"
-//           onChange={(e) => setMenu({ ...menu, date: e.target.value })}
-//         />
-
-//         <h3 className="mt-6 font-bold">Day</h3>
-//         <input className="input mt-2" placeholder="Rice"
-//           onChange={(e) => setMenu({ ...menu, day: { ...menu.day, rice: e.target.value } })}
-//         />
-//         <input className="input mt-2" placeholder="Pulse"
-//           onChange={(e) => setMenu({ ...menu, day: { ...menu.day, pulse: e.target.value } })}
-//         />
-//         <input className="input mt-2" placeholder="Veggie"
-//           onChange={(e) => setMenu({ ...menu, day: { ...menu.day, veggie: e.target.value } })}
-//         />
-
-//         <h3 className="mt-6 font-bold">Night</h3>
-//         <input className="input mt-2" placeholder="Rice"
-//           onChange={(e) => setMenu({ ...menu, night: { ...menu.night, rice: e.target.value } })}
-//         />
-//         <input className="input mt-2" placeholder="Pulse"
-//           onChange={(e) => setMenu({ ...menu, night: { ...menu.night, pulse: e.target.value } })}
-//         />
-//         <input className="input mt-2" placeholder="Veggie"
-//           onChange={(e) => setMenu({ ...menu, night: { ...menu.night, veggie: e.target.value } })}
-//         />
-
-//         <button onClick={submitMenu} className="btn mt-6">
-//           Save Menu
-//         </button>
-//       </div>
-
-//     </div>
-//   );
-// }
 
 const OwnerDashboard = () => {
   const navigate = useNavigate();
